7039a648cf914a08bfd3f172e3a00bf15ca98a14
JT <40304110+JThomasKern@users.noreply.github.com>
2024-12-15 22:30:17 -0800

    Update seasonalhours.js

--- seasonalhours.js
+++ seasonalhours.js
@@ -1,4 +1,4 @@
-// Me: Let's pretend there is no time at all.
+// Code adapted by JT
 
 // @see https://github.com/sgwilym/quadrantal-hours-clock/
 
@@ -6,15 +6,6 @@
  *  This concept originated in [Twodays Crossing](https://github.com/earthstar-project/twodays-crossing), an Earthstar chat app.
  *  This clock was created by [@cinnamon-bun](https://github.com/cinnamon-bun). We miss you.
  */
-
-/*
-  I loved the idea of the seasonal clock, which makes synchronised hours around the world by being linked to UTC.
-  But I am far from England, France, Germany, etc and the further away from UTC you go, the more evident the local temporality of many of the emojis.
-  So this clock removes those.
-  Normal day: morning/afternoon/evening/night
-  Seasonal day: spring/summer/fall/winter
-  atem day: ladderning, moosening, kitening, leafening
-*/
 const hourTable = {
     0: {
         quadrant: "Ladderning (🪜)",
